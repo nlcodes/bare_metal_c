db1160daeca40bc43c168cabe5e5b39dc8c9efb2
Nathan <nluskcodes@gmail.com>
2025-02-11 03:31:25 -0800

    Create driver which handles 4x4 button matrix gpio init; lights test led when any button in matrix is held, and turns it off upon release; accounts for each button inside an array where buttons s1-16 are mapped to array indexes 0-15; while any button is held its state is updated to 1 in the array and while not help its state is updated to 0

--- button_board_init/main.c
+++ button_board_init/main.c
@@ -3,30 +3,83 @@
 
 #define RCC_BASE 0x40023800
 #define GPIOB_BASE 0x40020400
-
 #define RCC_AHB1ENR (*(volatile uint32_t *)(RCC_BASE + 0x30))
 #define GPIOB_MODER (*(volatile uint32_t *)(GPIOB_BASE + 0x00))
 #define GPIOB_PUPDR (*(volatile uint32_t *)(GPIOB_BASE + 0x0C))
+
+/* Input register for gpiob
+ * Read only register which represents HIGH or LOW electrical state
+ */
 #define GPIOB_IDR (*(volatile uint32_t *)(GPIOB_BASE + 0x10))
 
+/* Writing register for changing state from HIGH or LOW */
+#define GPIOB_ODR (*(volatile uint32_t *)(GPIOB_BASE + 0x14))
+
+#define NUM_OF_BUTTONS 16
+
+int matrix_buttons[NUM_OF_BUTTONS] = {0};
+volatile uint8_t running = 1;
+
 int main() {
-  
+
   /* Enable GPIOB clock */
   RCC_AHB1ENR |= (1 << 1);
 
-  /* Set PB4 and PB5 input with pull up */
-  GPIOB_MODER &= ~(3 << 8);
-  GPIOB_PUPDR |= (1 << 8);
-  GPIOB_MODER &= ~(3 << 10);
-  GPIOB_PUPDR |= (1 << 10);
+  /* Set PB4-7 as inputs with pull-up (rows) */
+  /* Clear mode bits */
+  GPIOB_MODER &= ~(0xFF << 8);
 
-  while(1) {
-    if(!(GPIOB_IDR & (1 << 4))) {
-     hal_led_on_off(1);
-    } else if(!(GPIOB_IDR & (1 << 5))) {
-     hal_led_on_off(1);
-    } else {
-     hal_led_on_off(0);
-    }
+  /* Clear pullup bits first */
+  GPIOB_PUPDR &= ~(0xFF << 8);  
+  
+  /* Set pullup for PB4-7 */
+  GPIOB_PUPDR |= (0x55 << 8);   
+  
+  /* Set PB0-3 as outputs (columns) */
+  /* Clear bits 0-7 */
+  GPIOB_MODER &= ~(0xFF);
+
+  /* Set to output mode */
+  GPIOB_MODER |= 0x55; 
+
+  while(running) {
+
+    /* Columns */
+    for(int i = 0; i < 4; i++) {
+    GPIOB_ODR = ~(1 << i);  
+
+    for(volatile uint8_t i = 0; i < 100; i++);
+
+    uint32_t rows = GPIOB_IDR;
+
+      /* Rows
+       * Map all 4x4 button matrix to array
+       * Each button is represented as 0 or 1
+       * State changes to 1 while button is held
+       */
+      for(int j = 0; j < 4; j++) {
+        if(!(rows & (1 << (j + 4)))) {
+          matrix_buttons[(j * 4) + i] = 1;
+          hal_led_on_off(1);
+        } else {
+          matrix_buttons[(j * 4) + i] = 0;
+          hal_led_on_off(0);
+        }
+      }
+
+      /* Test array mapping
+       * All buttons tested pass test
+       * Mapping success
+       * Check notes.txt for pinout wiring
+       */
+      if(matrix_buttons[7] == 1) {
+        hal_led_on_off(1);
+        running = 0;
+      } else {
+        hal_led_on_off(0);
+      }
+    } 
   }
+
+  return 0;
 }
